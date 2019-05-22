f03a72b14e124e09298d29271b9480f9d9979380
sporternick <sporternick@gmail.com>
2019-05-22 11:52:56 +0200

    Initial commit

---
+++ Assets/scripts/canvasscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class canvasscript : MonoBehaviour, IPointerUpHandler
+{
+    int value;
+    TextMeshProUGUI goal, current;
+
+    void Start()
+    {
+        value = Random.Range(0, 100);
+        goal = GetComponentsInChildren<TextMeshProUGUI>()[0];
+        current = GetComponentsInChildren<TextMeshProUGUI>()[1];
+        goal.text = value.ToString();
+        current.text = GetComponentInChildren<Slider>().value.ToString();
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        check();
+    }
+
+    public void check()
+    {
+        if(value == GetComponentInChildren<Slider>().value)
+        {
+            goal.color = new Color(0, 255, 0);
+            Debug.Log("this is working");
+        }
+    }
+}
---
+++ Assets/scripts/textscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class textscript : MonoBehaviour
+{
+
+    int value;
+    TextMeshProUGUI objective;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objective = GetComponent<TextMeshProUGUI>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+   
+    public void cur(float current )
+    {
+        objective.text = current.ToString();
+    }
+}
