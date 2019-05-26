4eec5d50d44f2bc7eb4d0e1602ba115b6059edd4
Hugo Gil <47081772+HugDoggyDogg@users.noreply.github.com>
2019-05-26 20:40:34 +0200

    completed mainMenu, added gameController, changed CanvasScript to take values from controller

---
+++ Assets/scripts/CanvasScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class CanvasScript : MonoBehaviour, IPointerUpHandler
+{
+    
+    public TextMeshProUGUI goal, current, levelValue, gameModeValue;
+    
+    public GameController _gameController ;
+
+    public Slider slider;
+    
+
+    void Start()
+    {
+        //get the game controller
+        _gameController = GameObject.Find("gameController").GetComponent<GameController>();
+        
+        //get the slider
+        slider = GetComponentInChildren<Slider>();
+        
+        //set the UI variables according to the game controller
+        levelValue = GameObject.Find("level").GetComponent<TextMeshProUGUI>();
+        levelValue.text = _gameController.Level.ToString();
+        
+        gameModeValue = GameObject.Find("mode").GetComponent<TextMeshProUGUI>();
+        gameModeValue.text = _gameController.gamemode.ToString();
+
+        goal = GameObject.Find("goaltxt").GetComponent<TextMeshProUGUI>();
+        goal.text = "Reach " + _gameController.goalValue.ToString();
+        
+        current = GameObject.Find("currenttxt").GetComponent<TextMeshProUGUI>();
+        current.text = slider.value.ToString();
+        current.color = new Color(158, 158, 158, 0.3f);
+
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        check();
+    }
+
+    public void check()
+    {
+        Debug.Log("onpointerup, slider value :" + slider.value);
+        _gameController.CheckValue((int) slider.value); //send the slider value to the game controller to check if its correct
+        if (_gameController.levelCompleted)
+        {
+            current.color = new Color(0, 255, 0, 0.3f);
+        }
+    }
+
+    
+    
+    
+    
+}
--- Assets/scripts/mainMenu.cs
+++ Assets/scripts/mainMenu.cs
@@ -2,12 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+/**
+ * mainMenu
+ * a class that handles the main menu of the game
+ */
 public class mainMenu : MonoBehaviour
 {
-    public void starttest()
+    
+    //menu toggles
+    private Toggle gameModeToggleSpeed;
+    private Toggle gameControlToggleScreen;
+    private Toggle thumbSideToggle;
+    private GameController _gameController;
+
+
+    void Start()
     {
+        //get the game controller
+        _gameController = GameObject.Find("gameController").GetComponent<GameController>();
+        
+        //get the menu controls
+        thumbSideToggle = GameObject.Find("ThumbSideToggle").GetComponent<Toggle>();
+        gameModeToggleSpeed = GameObject.Find("SpeedyMode").GetComponent<Toggle>();
+        gameControlToggleScreen = GameObject.Find("ScreenControl").GetComponent<Toggle>();
+        
+        //set the game settings according to which toggles are enabled by default in Unity (property .isOn)
+        setThumbSide();
+        setGameControl();
+        setGameMode();
+    }
+    
+    public void setThumbSide()
+    {
+        _gameController.thumbside = thumbSideToggle.isOn ? ThumbSide.Left : ThumbSide.Right;
+    }
+    
+    public void setGameMode()
+    {
+        _gameController.gamemode = gameModeToggleSpeed.isOn ? GameMode.Speed : GameMode.Accuracy;
+    }
+    
+    public void setGameControl()
+    {
+        _gameController.gamecontrol = gameControlToggleScreen.isOn ? GameControl.Screen : GameControl.Radial;
+    }
+    
+    public void StartGame()
+    {       
         SceneManager.LoadScene("game");
     }
+    
    
 }
