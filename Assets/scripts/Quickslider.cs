8d0d790794136f8fd416a024ca65ab8ca258302d
sporternick <sporternick@gmail.com>
2019-05-27 10:01:47 +0200

    start of the modified slider

---
+++ Assets/scripts/Quickslider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quickslider : MonoBehaviour
+{
+   
+    
+   public void move()
+    {
+        Object circ = get
+        transform.position +=  new Vector3(transform.position.x +10, transform.position.y, transform.position.z);
+       
+    }
+
+}
