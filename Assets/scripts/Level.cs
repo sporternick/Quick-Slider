1aab2f3d94c93112951f4267a5d33c9795e7f993
sporternick <sporternick@gmail.com>
2019-06-05 12:04:56 +0200

    Revert "Merge branch 'gamification'"
    This reverts commit 96346eea8244bed27d7ceb0038232e9df6fca358, reversing
    changes made to 15fbdfe2b7be7a3d72e71ac716763b9b22cf1814.

--- Assets/scripts/Level.cs
+++ Assets/scripts/Level.cs
@@ -1,28 +1,23 @@
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Level
 {
-    private List<Round> _rounds ;
+    public List<Round> Rounds ;
     public int RoundNumber;
     public Round CurrentRound;
     public bool LevelCompleted = false;
-    private const int NbRounds = 5; //number of rounds to complete a level of the game
+    public int NbRounds = 10 ; //number of rounds to complete a level of the game
 
     public readonly Mode _mode;
     private readonly Difficulty _difficulty;
 
 
     public Level(Mode mode, Difficulty difficulty){
+        
         _mode = mode;
         _difficulty = difficulty;
-    }
-
-    public void InitLevel()
-    {
-        Debug.Log("Initiating level...");
-        LevelCompleted = false;
-        _rounds = new List<Round>();
+        
+        Rounds = new List<Round>();
         RoundNumber = 1;
         CurrentRound = NewRound();
     }
@@ -35,40 +30,15 @@ public class Level
                 (Round) new QuickRound(RoundNumber, (int) _difficulty) ;
     }
 
-    private void NextRound(){
-        if(_rounds.Count < NbRounds) _rounds.Add(CurrentRound);
+    public void NextRound(){
+        Rounds.Add(CurrentRound);
         RoundNumber++;
-        if (RoundNumber >= NbRounds)
-        {
-            TerminateLevel();
-        }
-        else
-        {
-            CurrentRound = NewRound();
-        }
+        CurrentRound = NewRound();
     }
 
-    private void TerminateLevel()
-    {
-        LevelCompleted = true;
-        ComputeScore();
-    }
+	
 
-    private void ComputeScore()
-    {
-        //use _rounds
-        Debug.Log("Score should be computed now.");
-    }
+	
 
-    public override string ToString()
-    {
-        return _mode + " level of difficulty " + _difficulty;
-    }
-
-    public int GetNbRounds()
-    {
-        return NbRounds;
-    }
-
-
+    
 }
--- Assets/scripts/QuickRound.cs
+++ Assets/scripts/QuickRound.cs
@@ -5,39 +5,23 @@
  */
 
 using System.Collections.Generic;
-using UnityEngine;
 
 public class QuickRound : Round
 {
 	
 	private static List<int> _accLevel = new List<int>() {4,2,1}; //Accuracy values for level (one-side threshold, i.e. +-threshold)
 
-	private readonly int _acc; //Accuracy : precision threshold level (0-2)
+	private int Acc; //Accuracy : precision threshold level (0-2)
 
-	public QuickRound(int currentRoundIndex, int difficulty) : base (currentRoundIndex)
+	public QuickRound(int index, int difficulty) : base (index)
 	{
-		_acc = _accLevel[difficulty];
-		Debug.Log("Created QuickRound, accuracy threshold = " + _acc);
+		Acc = _accLevel[difficulty];
 	}
 
-	protected override bool IsValueCorrect(int valueToCheck)
+	protected override bool IsValueCorrect(int playerValue)
 	{
-		int lowerGoal = Goal - _acc;
-		int upperGoal = Goal + _acc;
-		return valueToCheck >= lowerGoal && valueToCheck <= upperGoal;
-	}
-
-	public override void StartRound()
-	{
-		//starting cooldown
-		//start time counting
-		//wait for onPointerUp
-		//freeze, collect value
-		//complete
-	}
-
-	public override string GetInstructions()
-	{
-		return "Get as fast as possible to the value !";
+		int lowerGoal = Goal - Acc;
+		int upperGoal = Goal + Acc;
+		return playerValue >= lowerGoal && playerValue <= upperGoal;
 	}
 }
